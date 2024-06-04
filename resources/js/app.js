53aa004af97b184558c5306254ea511d1ae4965d
Marcus Cauê <cauemarcuscauefb@gmail.com>
2024-06-04 17:48:17 -0300

    Setup do repositório

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+/* Importação dos Popovers caso forem necessários */
+// import { Popover } from 'bootstrap';
+
+// const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+// const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new Popover(popoverTriggerEl))
